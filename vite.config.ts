530317cf4e0ab97c4df9514a335ce827017ca1b1
Hzd <205839042+Hzd-Gaming@users.noreply.github.com>
2025-06-17 08:09:30 +0700

    remove unused import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import { compression } from 'vite-plugin-compression2';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
